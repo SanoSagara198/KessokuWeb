0837790720f6366d314e75e51d25738adc13fcd1
SanoSagara198 <sanosuke298@gmail.com>
2026-08-06 17:18:31 -0300

    Install Kessoku presentation bridges

--- kessoku_site/__init__.py
+++ kessoku_site/__init__.py
@@ -1,6 +1,8 @@
 """Kessoku website presentation package."""
 
+from .bootstrap import install_polish_bridge
 from .media import install_media_bridge
 
 
+install_polish_bridge()
 install_media_bridge()
